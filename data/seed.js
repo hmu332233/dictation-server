a9e2d308bfb1003d974c70efd1db56e3250cb551
mu <hmu332233@gmail.com>
2017-08-22 02:06:06 +0900

    [수정] seed quiz 추가

--- data/seed.js
+++ data/seed.js
@@ -2,6 +2,7 @@ const mongoose   = require("mongoose");
 
 var Teacher = require("../models/teacher");
 var Student = require("../models/student");
+var Quiz = require("../models/quiz");
 
 
 /* 데이터 베이스 서버 연결 */
@@ -11,6 +12,7 @@ db.once('open', function(){
   console.log("Connected to mongod server");
 	
 	var teacher = {
+		_id: "599b03151c6e6f0159a72815",
 		email: "test@test.com",
 		school: "한국초등학교",
 		name: "한의현"
@@ -28,6 +30,52 @@ db.once('open', function(){
 		
 	};
 	
+	var quiz = {
+		"number": 9,
+		"questions": [
+			{
+				"number": 1,
+				"sentence": "세수를 합니다."
+			},
+			{
+				"number": 2,
+				"sentence": "잠을 잡니다."
+			},
+			{
+				"number": 3,
+				"sentence": "책을 읽습니다."
+			},
+			{
+				"number": 4,
+				"sentence": "꼬리를 흔듭니다."
+			},
+			{
+				"number": 5,
+				"sentence": "시소 미끄럼틀"
+			},
+			{
+				"number": 6,
+				"sentence": "놀이터에서 놀아요."
+			},
+			{
+				"number": 7,
+				"sentence": "그네를 타요."
+			},
+			{
+				"number": 8,
+				"sentence": "콩쥐가 울고"
+			},
+			{
+				"number": 9,
+				"sentence": "항아리가 깨졌습니다."
+			},
+			{
+				"number": 10,
+				"sentence": "다람쥐가 도와줍니다."
+			}
+		]
+	};
+	
 	Teacher.create(teacher, function (err, teacher){
 		if(err) return console.log(err);
 		console.log(teacher);
@@ -37,8 +85,14 @@ db.once('open', function(){
 			console.log(student);
 			teacher.students.push(student._id);
 			teacher.save();
-			db.close();
+			Quiz.create(quiz, function (err, quiz){
+			if(err) return console.log(err);
+				console.log(quiz);
+				db.close();
+			});
 		});
+		
+		
 	});
 	
 });

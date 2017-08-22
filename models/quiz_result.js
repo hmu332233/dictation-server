c1a0bc49f68c8ea65409f164fda76342f1730d90
mu <hmu332233@gmail.com>
2017-08-23 02:27:58 +0900

    [수정] date fotmat 변경

--- models/quiz_result.js
+++ models/quiz_result.js
@@ -1,3 +1,5 @@
+require('../util/date');
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
@@ -5,7 +7,7 @@ var mongoose = require('mongoose'),
 /* 스키마 정의 */
 var quizResultSchema = new Schema({
     //id는 기본으로 생성됨
-		date: { type: Date, default: Date.now },
+		date: { type: String, default: new Date().format("MM월 dd일 a/p hh시 mm분")},
 		quiz_number: Number, //quiz 외래키
 		student_name: String,
 		score: Number,

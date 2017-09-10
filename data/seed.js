c8b78f90358a5d183d7f257e3b4a43af079de69f
mu <hmu332233@gmail.com>
2017-09-10 16:20:24 +0900

    [수정] seed 내용 추가

--- data/seed.js
+++ data/seed.js
@@ -1,9 +1,10 @@
 const mongoose   = require("mongoose");
 
+var async = require('async');
 var Teacher = require("../models/teacher");
 var Student = require("../models/student");
 var Quiz = require("../models/quiz");
-
+var QuizHistory = require("../models/quiz_history");
 
 /* 데이터 베이스 서버 연결 */
 var db = mongoose.connection;
@@ -19,15 +20,21 @@ db.once('open', function(){
 	};
 	
 	var student1 = {
+		_id: "599b4041cefd5d07c5be9594",
 		school: "한국초등학교",
 		level: "1",
 		class_name: "2",
 		student_id: 1,
-		name: "반도범",
+		name: "반도범"
 	};
 	
 	var student2 = {
-		
+		_id: "599c75f7836cc308789f5902",
+		school: "한국초등학교",
+		level: "1",
+		class_name: "2",
+		student_id: 2,
+		name: "한민웅"
 	};
 	
 	var quiz = {
@@ -77,25 +84,90 @@ db.once('open', function(){
 		]
 	};
 	
-	Teacher.create(teacher, function (err, teacher){
-		if(err) return console.log(err);
-		console.log(teacher);
-		
- 		Student.create(student1, function (err, student){
-			if(err) return console.log(err);
-			console.log(student);
-			teacher.students.push(student._id);
-			teacher.save();
-			Quiz.create(quiz, function (err, quiz){
-			if(err) return console.log(err);
-				console.log(quiz);
-				db.close();
-			});
-		});
-		
-		
-	});
+	var quiz_history = {
+		"teacher_id": "599b03151c6e6f0159a72815",
+		"quiz_number": 1,
+		"_id": "599c85332a7fdd0961a8c712"
+	};
 	
+	var tasks2 = [
+		function(callback){
+			QuizHistory.create(quiz_history, function (err, quizHistory){
+				if(err) callback(err, 0);
+				console.log('quiz history save');
+				callback(null, quizHistory);
+			});
+		},
+		function(data, callback){
+			Teacher.findById(teacher._id, function (err, teacher){
+				if(err) callback(err, 0);
+				if(!teacher) console.log('not find teacher');
+				teacher.quiz_histories.push(data._id);
+				teacher.save();
+				console.log('teacher add quiz history');
+				callback(null, data._id);
+			});
+		}
+	];
+	
+	var tasks = [
+		function (callback) {
+			Teacher.remove({}, function (err){
+				Student.remove({}, function (err){
+					Quiz.remove({}, function (err){
+						QuizHistory.remove({}, function (err){
+							console.log('remove all success');
+							callback(null,0);
+						});
+					});
+				});
+			});
+		},
+		function (data, callback){
+			Teacher.create(teacher, function (err, teacher){
+				if(err) return console.log(err);
+				console.log('teacher save');
+				callback(null,teacher);
+			});
+		},
+		function (teacher, callback){
+			Student.create(student2, function (err, student){
+				console.log('student2 save');
+				teacher.students.push(student._id);
+				teacher.save();
+				callback(null,teacher);
+			});
+		},
+		function (teacher, callback){
+			Student.create(student1, function (err, student){
+				if(err) return console.log(err);
+				console.log('student1 save');
+				teacher.students.push(student._id);
+				teacher.save();
+				callback(null,teacher);
+			});
+		},
+		function (teacher, callback){
+			Quiz.create(quiz, function (err, quiz){
+				if(err) return console.log(err);
+				console.log('quiz save');
+				callback(null,teacher);
+			});	
+		},
+		function (data, callback){
+			async.waterfall(tasks2, function (err, results){
+				if(err) return res.status(500).send(err);
+				console.log('quiz history save');
+				callback(null,0);
+			});
+		}
+	];
+	
+	
+	async.waterfall(tasks, function (err, results){
+		if(err) return res.status(500).send(err);
+		console.log('seed complete');
+	});
 });
 mongoose.connect('mongodb://localhost:27017/dictationdb');
 

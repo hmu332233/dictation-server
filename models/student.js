b8364a0d64dd070065be57ad859cbf251094dc14
mu <hmu332233@gmail.com>
2017-09-14 22:31:40 +0900

    [추가] 학생 중복 체크 함수

--- models/student.js
+++ models/student.js
@@ -13,4 +13,14 @@ var studentSchema = new Schema({
 		quiz_results: Array
 });
 
+/* 중복체크 함수 */
+studentSchema.statics.checkForDuplicate = function (data, callback) {
+  this.findOne({school: data.school, grade: data.grade, class: data.class, student_id: data.student_id}, function (err, student){
+		if(err) callback(err, null);
+		
+		if(!student) callback(null, false);
+		else callback(null, true);
+	});
+};
+
 module.exports = mongoose.model("Student", studentSchema);

f178b1a0caac1b61fd30badfe2f615c9250d726e
mu <hmu332233@gmail.com>
2017-09-27 02:12:31 +0900

    [추가] run-script seed-school

--- data/save.js
+++ data/save.js
@@ -3,20 +3,27 @@ var School = require("../models/school");
 const mongoose   = require("mongoose");
 
 /* 데이터 베이스 서버 연결 */
+mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function(){
   console.log("Connected to mongod server");
-	
-	fs.readFile('school_data.txt','utf8', function(err, data){
+
+	fs.readFile('./data/school_data.txt','utf8', function(err, data){
 		var schools = JSON.parse(data);
-		for(var i in schools){
-			var school = schools[i];
-			School.create(school, function(err, sc){
-				console.log(sc);
-				db.close();
-			});
-		}
+    
+    School.remove({}).then(function (){
+      console.log('schools data remove all');
+      for(var i in schools){
+        var school = schools[i];
+        School.create(school, function(err, sc){
+          console.log(sc);
+          db.close();
+        });
+      }
+      console.log('schools data create complete');
+    });
+		
 	});
 });
 mongoose.connect('mongodb://localhost:27017/dictationdb');

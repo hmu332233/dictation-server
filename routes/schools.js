f705a9f4b1c022b8e6fe4dcdb975768c507f2577
mu <hmu332233@gmail.com>
2017-09-28 23:17:12 +0900

    [추가] school search

--- routes/schools.js
+++ routes/schools.js
@@ -3,10 +3,35 @@ const router = express.Router();
 
 var School = require("../models/school");
 
+//search
+router.get("/schools/search", function (req, res){
+  var query = {};
+  var _region1 = req.query.region1;
+  var _region2 = req.query.region2;
+  
+  if(_region1) query.region1 = _region1;
+  if(_region2) query.region2 = _region2;
+  
+  console.log(query);
+  
+  School.find(query, function (err, schools){
+    if(err){
+      console.err(err);
+      return res.status(500).send(err);
+    } 
+    if(schools.length === 0) return res.status(404).send({});
+    
+    return res.send(schools);
+  });
+});
+
 //index
 router.get("/schools", function (req, res){
 	School.find({}, function (err, schools){
-		if(err) return res.status(500).send(err);
+		if(err){
+      console.log(err);
+      return res.status(500).send(err);
+    }
 		res.send(schools);
 	});
 });

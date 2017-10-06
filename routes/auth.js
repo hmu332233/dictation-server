0fe12c29e4b8cd366163932c03e67952c69e3cab
mu <hmu332233@gmail.com>
2017-10-06 21:15:48 +0900

    [Modify] 학생 로그인 동작 방식 변경

--- routes/auth.js
+++ routes/auth.js
@@ -4,38 +4,42 @@ const router = express.Router();
 var Teacher = require("../models/teacher");
 var Student = require("../models/student");
 
+/*
+학생 또는 선생님 로그인
+
+type = 선생님
+@params = {login_id, password}
+
+type = 학생
+@params = {school, grade, class, student_id, name}
+*/
 router.post('/auth/login', function (req, res){
 	
-	var login_id = req.body.login_id;
-	var pw = req.body.password;
 	var type = req.body.type;
+  delete req.body.type;
 	
-	// console.log(email);
-	// console.log(pw);
-	// console.log(type);
 	switch(type) {
     case 'teacher':
-			Teacher.findOne({login_id: login_id, password: pw}, function (err, teacher){
+			Teacher.findOne(req.body, function (err, teacher){
 				if(err) return res.status(500).send(err);
 				if(!teacher) return res.status(404).send({});
 				
 				res.send(teacher);
 			});
       break;
-    case 'student':
-			
-			var name = req.body.name;
-			var school = req.body.school;
-			var student_id = req.body.student_id;
-			
-			Student.findOne({name: name, school: school, student_id: student_id}, function(err, student){
-				if(err) return res.status(500).send(err);
-  			if(!student) return res.status(404).send({result: "fail",user:{}});
-				res.send({result: "success",type:"student",user:student});
+    case 'student':  
+			Student.findOne(req.body, function(err, student){
+				if(err) {
+        	return res.status(500).send(err);
+        }
+  			if(!student) {
+        	return res.status(404).send({});
+        }
+				res.send(student);
 			});
     	break;
     default:
-			res.status(400).send({result:'fail',user:{}});
+			res.status(400).send({});
 	}
 });
 

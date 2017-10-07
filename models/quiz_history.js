2d58d5eda14022928a7e3d8679b8de1976de346b
mu <hmu332233@gmail.com>
2017-10-08 02:51:51 +0900

    [Modify] QuizHistory에 RectifyCount 추가

--- models/quiz_history.js
+++ models/quiz_history.js
@@ -9,7 +9,19 @@ var quizHistorySchema = new Schema({
     //id는 기본으로 생성됨
 		date: { type: String, default: new Date().format("MM월 dd일 a/p hh시 mm분")},
 		quiz_number: Number, //quiz 외래키
-		quiz_results: Array
+		quiz_results: Array,
+  	rectify_count: {
+      property1: { type: Number, default: 0 },
+      property2: { type: Number, default: 0 },
+      property3: { type: Number, default: 0 },
+      property4: { type: Number, default: 0 },
+      property5: { type: Number, default: 0 },
+      property6: { type: Number, default: 0 },
+      property7: { type: Number, default: 0 },
+      property8: { type: Number, default: 0 },
+      property9: { type: Number, default: 0 },
+      property10: { type: Number, default: 0 }
+    }
 });
 
 /*

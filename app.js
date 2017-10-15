3a2db3d0a5a762a22cf9f9c86d100582296453fd
mu <hmu332233@gmail.com>
2017-10-16 00:11:52 +0900

    [Add] 개발용 log 추가

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ const bodyParser = require('body-parser');
 const mongoose   = require("mongoose");
 const logger = require('morgan');
 
-/*개발용 log*/
-app.use(logger('dev'));
+const node_env = process.env.NODE_ENV || 'development';
+
 /*bodyParser*/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+/*개발용 log*/
+if(node_env === 'development'){
+	app.use(logger('dev'));
+	app.use(function (req, res, next) {
+		console.log(req.body);
+		next();
+  });
+}
 
 /* 데이터 베이스 서버 연결 */
 var db = mongoose.connection;
@@ -19,6 +28,7 @@ db.once('open', function(){
 });
 mongoose.connect( process.env.DB_URL || 'mongodb://localhost:27017/dictationdb');
 mongoose.Promise = global.Promise;
+
 
 /*route*/
 app.use('/', require('./routes/auth'));

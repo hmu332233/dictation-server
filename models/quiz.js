1030378f5aecadb269537a3fc75c8e6ffdf09cd7
mu <hmu332233@gmail.com>
2017-10-15 15:29:33 +0900

    [Modify] quiz.number에 auto_increment 적용

--- models/quiz.js
+++ models/quiz.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+		Schema = mongoose.Schema,
+		autoIncrement = require('mongoose-auto-increment');
 
+autoIncrement.initialize(mongoose.connection);
 
 /* 스키마 정의 */
 var quizSchema = new Schema({
@@ -8,6 +10,14 @@ var quizSchema = new Schema({
 		number: { type: Number, unique: true},
 		name: String,
 		questions: Array
+});
+
+//number에 대해 auto increment를 적용한다.
+quizSchema.plugin(autoIncrement.plugin, {
+	model: 'Quiz',
+	field: 'number',
+	startAt: 1,
+	incrementBy: 1
 });
 
 module.exports = mongoose.model("Quiz", quizSchema);
